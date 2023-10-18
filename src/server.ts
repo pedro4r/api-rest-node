8b96b84aafda15014d7b9dd2337db66ba051a6cc
Pedro Requiao <alonsofts@gmail.com>
2023-10-18 17:18:54 -0400

    add: Zod

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,30 @@
 import fastify from 'fastify'
 import { knex } from './database'
+import { randomUUID } from 'crypto'
+import { env } from './env'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-    const tables = await knex('sqlite_schema').select('*')
-    return tables
+    const transaction = await knex('transactions')
+        .where('amount', 1000)
+        .select('*')
+
+    // const transaction = await knex('transactions').select('*')
+
+    // const transaction = await knex('transactions')
+    //     .insert({
+    //         id: randomUUID(),
+    //         title: 'Test Transaction',
+    //         amount: 1000,
+    //     })
+    //     .returning('*')
+
+    return transaction
 })
 
 app.listen({
-    port: 3333,
+    port: env.PORT,
 }).then(() => {
     console.log('HTTP Server Running')
 })
