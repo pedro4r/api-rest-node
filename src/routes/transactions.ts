b6ae154d2b7880b675847c177b7ea6e835437b77
Pedro Requiao <alonsofts@gmail.com>
2023-10-31 18:11:16 -0400

    add: Transactions list route

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -4,6 +4,25 @@ import { randomUUID } from 'node:crypto'
 import { knex } from '../database'
 
 export async function TransactionsRoutes(app: FastifyInstance) {
+    app.get('/', async () => {
+        const transactions = await knex('transactions').select()
+        return transactions
+    })
+
+    app.get('/:id', async (request) => {
+        const getTransactionParamsSchema = z.object({
+            id: z.string().uuid(),
+        })
+
+        const { id } = getTransactionParamsSchema.parse(request.params)
+
+        const transaction = await knex('transactions').where('id', id).first()
+
+        return {
+            transaction,
+        }
+    })
+
     app.post('/', async (request, reply) => {
         // {title, amount, type: credit of debit}
         const createTransactionBodySchema = z.object({
