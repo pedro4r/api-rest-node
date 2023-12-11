9e21d40a7b05ad2b84044871cdc6dd4756417018
Pedro Requiao <alonsofts@gmail.com>
2023-12-11 00:49:15 -0500

    add: New transaction automated testing

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,5 @@
-import fastify from 'fastify'
+import { app } from './app'
 import { env } from './env'
-import { TransactionsRoutes } from './routes/transactions'
-import cookie from '@fastify/cookie'
-
-const app = fastify()
-
-app.register(cookie)
-
-app.addHook('preHandler', async (request, reply) => {
-    console.log(`[${request.method}] ${request.url}`)
-})
-
-app.register(TransactionsRoutes, {
-    prefix: 'transactions',
-})
 
 app.listen({
     port: env.PORT,
