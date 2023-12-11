2712ffa01798484f5bb9bbab1f473c61cc4f757b
Pedro Requiao <alonsofts@gmail.com>
2023-12-11 01:14:23 -0500

    add: Listing transactions automated testing

--- test/transactions.test.ts
+++ test/transactions.test.ts
@@ -1,4 +1,4 @@
-import { afterAll, beforeAll, test, describe } from 'vitest'
+import { afterAll, beforeAll, it, describe, expect } from 'vitest'
 import request from 'supertest'
 import { app } from '../src/app'
 
@@ -11,7 +11,7 @@ describe('Transactions routes', () => {
         await app.close()
     })
 
-    test('use can create a new transaction', async () => {
+    it('should be able create a new transaction', async () => {
         await request(app.server)
             .post('/transactions')
             .send({
@@ -20,5 +20,29 @@ describe('Transactions routes', () => {
                 type: 'credit',
             })
             .expect(201)
+    })
+
+    it('should be able to list transactions', async () => {
+        const createTransactionResponse = await request(app.server)
+            .post('/transactions')
+            .send({
+                title: 'New transaction',
+                amount: 5000,
+                type: 'credit',
+            })
+
+        const cookies = createTransactionResponse.get('Set-Cookie')
+
+        const listTransactionsResponse = await request(app.server)
+            .get('/transactions')
+            .set('Cookie', cookies)
+            .expect(200)
+
+        expect(listTransactionsResponse.body.transactions).toEqual([
+            expect.objectContaining({
+                title: 'New transaction',
+                amount: 5000,
+            }),
+        ])
     })
 })
